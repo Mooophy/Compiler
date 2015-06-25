c561c73c7265ed2202048e365432ba616d794aac
Mooophy <moophyalan@gmail.com>
2015-06-25 17:52:10 +1200

    polish

--- Dragon/Source/Token.cs
+++ Dragon/Source/Token.cs
@@ -81,16 +81,16 @@ namespace Dragon
         }
 
         public readonly static Word
-            and = new Word("&&", Tag.AND),
-            or = new Word("||", Tag.OR),
-            eq = new Word("==", Tag.EQ),
-            ne = new Word("!=", Tag.NE),
-            le = new Word("<=", Tag.LE),
-            ge = new Word(">=", Tag.GE),
-            minus = new Word("minus", Tag.MINUS),
-            True = new Word("true", Tag.TRUE),
-            False = new Word("false", Tag.FALSE),
-            temp = new Word("t", Tag.TEMP);
+            and     = new Word("&&", Tag.AND),
+            or      = new Word("||", Tag.OR),
+            eq      = new Word("==", Tag.EQ),
+            ne      = new Word("!=", Tag.NE),
+            le      = new Word("<=", Tag.LE),
+            ge      = new Word(">=", Tag.GE),
+            minus   = new Word("minus", Tag.MINUS),
+            True    = new Word("true", Tag.TRUE),
+            False   = new Word("false", Tag.FALSE),
+            temp    = new Word("t", Tag.TEMP);
     }
 
 
