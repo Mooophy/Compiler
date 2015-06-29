879a6f3cd3523dfa65e3a6ce88fd20c7bd426cc0
Mooophy <moophyalan@gmail.com>
2015-06-29 17:39:59 +1200

    testing Constant 	modified:   Dragon/UnitTests/TestConstant.cs

--- Dragon/UnitTests/TestConstant.cs
+++ Dragon/UnitTests/TestConstant.cs
@@ -8,9 +8,20 @@ namespace UnitTests
     public class TestConstant
     {
         [TestMethod]
+        public void TestConstantStaticMembers()
+        {
+            Assert.AreEqual("true", Constant.True.ToString());
+            Assert.AreEqual("false", Constant.False.ToString());
+        }
+
+        [TestMethod]
         public void TestConstantCtors()
         {
+            var c1 = new Constant(42);
+            Assert.AreEqual("42", c1.ToString());
 
+            var c2 = new Constant(new Real(3.14f), Dragon.Type.Int);
+            Assert.AreEqual("3.14", c2.ToString());
         }
     }
 }
