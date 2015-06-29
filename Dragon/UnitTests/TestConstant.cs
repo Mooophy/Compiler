137cbe6a7a8103fd856c89db4bb8214d93a59a0e
Mooophy <moophyalan@gmail.com>
2015-06-29 17:28:57 +1200

    new file:   Dragon/UnitTests/TestConstant.cs

---
+++ Dragon/UnitTests/TestConstant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dragon;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class TestConstant
+    {
+        [TestMethod]
+        public void TestConstantCtors()
+        {
+
+        }
+    }
+}
