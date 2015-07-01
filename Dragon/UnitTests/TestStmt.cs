64f02137e80ac965c8f1330e3177be6bc278433c
Mooophy <moophyalan@gmail.com>
2015-07-01 13:03:00 +1200

    tested Stmt

---
+++ Dragon/UnitTests/TestStmt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dragon;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class TestStmt
+    {
+        [TestMethod]
+        public void TestStmtClass()
+        {
+            var stmt = new Stmt();
+            Assert.AreEqual(Stmt.Null, Stmt.Enclosing);
+        }
+    }
+}
