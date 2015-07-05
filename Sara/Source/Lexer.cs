a6e859a3403a2ce0fa13abb36c2e11bb3f2f2d2a
moophy <MoophyAlan@gmail.com>
2015-07-06 06:23:44 +1200

    working on Lexer

--- Sara/Source/Lexer.cs
+++ Sara/Source/Lexer.cs
@@ -2,39 +2,49 @@
 using System.IO;
 using System.Text;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Sara
 {
+    public class Words : Dictionary<string, Word>
+    {
+        private readonly List<Word> _keyWords = new List<Word>
+        {
+                new Word("if", Tag.IF),
+                new Word("else", Tag.ELSE),
+                new Word("while", Tag.WHILE),
+                new Word("do", Tag.DO),
+                new Word("break", Tag.BREAK),
+
+                Word.True,
+                Word.False,
+                Type.Int,
+                Type.Char,
+                Type.Bool,
+                Type.Float
+        };
+
+        public List<Word> KeyWords
+        {
+            get { return _keyWords; }
+        }
+
+        public Words()
+        {
+            _keyWords.ForEach(w => { this[w.Lexeme] = w; });
+        }
+    }
+
     public class Lexer
     {
         private StreamReader _stream;
         public Dictionary<string, Word> KeyWords { get; private set; }
         public long Line { get; private set; }
 
-        private void InitKeyWords(Dictionary<string, Word> dic)
-        {
-            Action<Word> reserve = w => { dic.Add(w.Lexeme, w); };
-
-            reserve(new Word("if", Tag.IF));
-            reserve(new Word("else", Tag.ELSE));
-            reserve(new Word("while", Tag.WHILE));
-            reserve(new Word("do", Tag.DO));
-            reserve(new Word("break", Tag.BREAK));
-            reserve(Word.True);
-            reserve(Word.False);
-
-            //need class Type to before uncomment
-            //reserve(Type.Int);
-            //reserve(Type.Char);
-            //reserve(Type.Bool);
-            //reserve(Type.Float);
-        }
-
         public Lexer(StreamReader sreader)
         {
             this._stream = sreader;
-            this.KeyWords = new Dictionary<string, Word>();
-            this.InitKeyWords(this.KeyWords);
+            this.KeyWords = new Sara.Words();
             this.Line = 1;
         }
     }
---
+++ Sara/Source/Symbols.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Sara
+{
+    public class Type : Word
+    {
+        public int Width;
+
+        public Type(string typeName, char tag, int width)
+            : base(typeName, tag)
+        {
+            this.Width = width;
+        }
+
+        public readonly static Type
+            Int = new Type("int", Tag.BASIC, 4),
+            Float = new Type("float", Tag.BASIC, 8),
+            Char = new Type("char", Tag.BASIC, 1),
+            Bool = new Type("bool", Tag.BASIC, 1);
+
+        public static bool Numeric(Type type)
+        {
+            return type == Type.Char || type == Type.Int || type == Type.Float;
+        }
+
+        public static Type Max(Type lhs, Type rhs)
+        {
+            if (!Type.Numeric(lhs) || !Type.Numeric(rhs))
+                return null;
+            else if (lhs == Type.Float || rhs == Type.Float)
+                return Type.Float;
+            else if (lhs == Type.Int || rhs == Type.Int)
+                return Type.Int;
+            else
+                return Type.Char;
+        }
+    }
+}
