62df91da3ddc6116229a547810823378f0c8bea3
moophy <MoophyAlan@gmail.com>
2015-07-06 14:05:09 +1200

    working on Lexer

--- Sara/Source/Lexer.cs
+++ Sara/Source/Lexer.cs
@@ -40,6 +40,7 @@ namespace Sara
         private StreamReader _stream;
         public Dictionary<string, Word> Words { get; private set; }
         public long Line { get; private set; }
+        public IList<Token> Result { get; private set; }
 
         public Lexer(StreamReader sr)
         {
@@ -47,5 +48,53 @@ namespace Sara
             this.Words = new Sara.WordTable();
             this.Line = 1;
         }
+
+        private IList<Token> Scan(StreamReader reader)
+        {
+            //delegates for later use
+            Func<char> ch       = () => { return (char)reader.Peek(); };
+            Func<bool> notEof   = () => { return reader.Peek() != -1; };
+            Func<bool> isWS     = () => { return char.IsWhiteSpace(ch()); };
+            Func<bool> isWord   = () => { return char.IsLetter(ch()) || '_' == ch(); };
+            Action     move     = () => { reader.Read(); };
+            Func<char, bool> matchNext = (char arg) => { move(); return arg == ch(); };
+            var ret = new List<Token>();
+
+            while(notEof())
+            {
+                for (; isWS(); move()) ;
+
+                //for operators like && !=, etc
+                switch(ch())
+                {
+                    case '&': 
+                        ret.Add(matchNext('&') ? Word.and : new Token('&')); break;
+                    case '|': 
+                        ret.Add(matchNext('|') ? Word.or  : new Token('|')); break;
+                    case '=':
+                        ret.Add(matchNext('=') ? Word.eq  : new Token('=')); break;
+                    case '!':
+                        ret.Add(matchNext('=') ? Word.ne  : new Token('!')); break;
+                    case '<':
+                        ret.Add(matchNext('=') ? Word.le  : new Token('<')); break;
+                    case '>':
+                        ret.Add(matchNext('=') ? Word.ge  : new Token('>')); break;
+                }
+
+                //for identifiers and reserved words
+                if(isWord())
+                {
+                    var sb = new StringBuilder();
+                    sb.Append(ch());
+                    move();
+                    for (; isWord() || char.IsDigit(ch()); move()) sb.Append(ch());
+                    var w = sb.ToString();
+
+                    if (this.Words.ContainsKey(w)) this.Result.Add(Words[w]);
+                }
+            }
+            return ret;
+        }
+
     }
 }
